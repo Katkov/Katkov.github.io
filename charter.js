59b13f932d75d1afcee81a9a6bdcc7eea699188f
michaelkatkov <eloorus@gmail.com>
2018-01-17 20:15:33 +0200

    added test json object

---
+++ charter.js
@@ -0,0 +1,28 @@
+{
+  "timestamp": "2018-01-09T15:04:36.502-0600",
+  "returnString": "Success",
+  "roundTrip": 27,
+  "returnCode": 0,
+  "lastActivityTimestamp": "2018-01-09T12:32:15.192-0600",
+  "accountId": "8245123940127564",
+  "responseUuid": "008d6651-fa71-44b9-8293-994bd6a5d780",
+  "serviceIds": [
+    "TPV6366512413"
+  ],
+  "stalledServiceIds": [
+    "TPV6366512413"
+  ],
+  "contractIds": [
+    "16c21f22-167c-4d4b-a618-9f907223a4dd"
+  ],
+  "processing": false,
+  "messages": [
+    {
+      "createdTimestamp": "2018-01-09T12:38:27.873-0600",
+      "uuid": "1409f28b-ef8f-49bf-ae1a-2aff8ef939ce",
+      "contractId": "16c21f22-167c-4d4b-a618-9f907223a4dd",
+      "messageType": "ERROR",
+      "messageText": "The contract stalled."
+    }
+  ]
+}
